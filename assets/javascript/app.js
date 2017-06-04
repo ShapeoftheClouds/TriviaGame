d347fc1c20d73beeb2acb26840be22ed8a67895c
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-06-03 23:00:13 -0600

    screen shows question and choices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,4 @@
-// My PseudoCODE!!!
-
-// Have a series of questions and answers.
-
-
+// My variables
 var correctAnswers = 0; 
 var wrongAnswers = 0;
 var countDown = 31;
@@ -11,78 +7,118 @@ var appIsRunning = false;
 var nextQuestion = false;
 var currentQuestion;
 
-
 // Objects have properties and values.
 // Refactoring - Rewritting code. Commit often. 
 
-// My questions as an object. 
+// My questions as an array of objects 
 var questions = [
 	{
-		question: "What color was Spock's skin originally going to be?",
-		answer: ["Blue", "Green", "Red", "Orange"],
+		question: "What color was Spock's skin color originally going to be?",
+		answer: ["BLUE", "GREEN", "RED", "ORANGE"],
 		correctAnswer: 2
 	}, {
 		question: "Which character's actor originated the Klingon language?",
 		// All possible answer options are listed in an array.
-		answer: ["Dr. McCoy", "Spock", "Uhura", "Scotty"],
+		answer: ["DR. MCCOY", "SPOCK", "UHURA", "SCOTTY"],
 		correctAnswer: 3
 	}, {
 		question: "Which Star Trek series had the highest ratings of any Star Trek series?",
 		// All possible answer options are listed in an array.
-		answer: ["TOS", "Voyager", "TNG", "DS9"],
+		answer: ["TOS", "VOYAGER", "TNG", "DS9"],
 		correctAnswer: 2 
 	}, {
 		question: "What were the Borg originally supposed to be designed after?",
 		// All possible answer options are listed in an array.
-		answer: ["Cactus", "Birds", "Worms", "Insects"], 
+		answer: ["CACTUS", "BIRDS", "WORMS", "INSECTS"], 
 		correctAnswer: 3
 	}, {
 		question: "What does the name of the alien race Ferengi mean?",
 		// All possible answer options are listed in an array.
-		answer: ["Settlers", "Foreigner", "Outcast", "Wanderer"], 
+		answer: ["SETTLERS", "FOREIGNER", "OUTCAST", "WANDERER"], 
 		correctAnswer: 1
 	},
 ];
-
-// var questionOne = questions[1].answer[0];
-// console.log(questionOne);
 
 // After the document loads
 $(document).ready(function(){
 
 // Display the start button
-
 	$(".startButton").on("click", function(){
 		$("#image").hide();
 		$(".startButton").hide();
 		theTimer();
+		displayQuestionOne();
 	});
-
 });
 
+
+// The Timer. This function decreased the number by a second. 
 function theTimer() {
 	decreasingNum = setInterval(decrement, 1000);
 };
 
-
+// Subtracts from the count down number, shows the timer in the htmla, and stops the interval from decreasing once it reaches zero.
 function decrement() {
 	countDown--;
 	$("#timer").html(countDown);
-
 	if (countDown === 0) {
 		clearInterval(decreasingNum);
 	}
 };
 
-// Creating a 30 second timer
-// function timerCountdown() {
-// 	var timeRemaining = seconds = timer
-// 	$("#timer").append(counter);	
-// }
+function rightAnswerOne() {
+	$("#answer").html("A most logical response!");
+};
 
-// function questions() {
 
-// }
+
+// Wrong answer
+function wrongAnswerOne() {
+
+};
+
+
+// Displaying questions. There's most likely a way to simplify this code. Look into it.
+function displayQuestionOne() {
+	$("#question").html(questions[0].question);
+	$("#optionOne").html(questions[0].answer[0]);
+	$("#optionTwo").html(questions[0].answer[1]);
+	$("#optionThree").html(questions[0].answer[2]);
+	$("#optionFour").html(questions[0].answer[3]);
+};
+
+function displayQuestionTwo() {
+	$("#question").html(questions[1].question);
+	$("#optionOne").html(questions[1].answer[0]);
+	$("#optionTwo").html(questions[1].answer[1]);
+	$("#optionThree").html(questions[1].answer[2]);
+	$("#optionFour").html(questions[1].answer[3]);
+};
+
+function displayQuestionThree() {
+	$("#question").html(questions[2].question);
+	$("#optionOne").html(questions[2].answer[0]);
+	$("#optionTwo").html(questions[2].answer[1]);
+	$("#optionThree").html(questions[2].answer[2]);
+	$("#optionFour").html(questions[2].answer[3]);
+};
+
+function displayQuestionFour() {
+	$("#question").html(questions[3].question);
+	$("#optionOne").html(questions[3].answer[0]);
+	$("#optionTwo").html(questions[3].answer[1]);
+	$("#optionThree").html(questions[3].answer[2]);
+	$("#optionFour").html(questions[3].answer[3]);
+};
+
+function displayQuestionFive() {
+	$("#question").html(questions[4].question);
+	$("#optionOne").html(questions[4].answer[0]);
+	$("#optionTwo").html(questions[4].answer[1]);
+	$("#optionThree").html(questions[4].answer[2]);
+	$("#optionFour").html(questions[4].answer[3]);
+};
+
 
 // Show start button.
 // On button click, start game. 
